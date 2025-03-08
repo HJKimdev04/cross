b182bcd7af3105c7347ec396e8812c566d4c0a3a
HJKimdev04 <wva22lfr@kakao.com>
2025-03-08 17:54:44 +0900

    Update Square.js

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import '../styles/Square.css'
+import '../styles/Square.css'; 
 
 const Square = ({ isBlack, onChange }) => {
     return (
-        <div className={`square ${isBlack ? 'black' : ''}`}>
-            {!isBlack && <input type="text" maxLength="1" onChange={onChange} />}
+        <div className={`square ${isBlack ? 'black' : 'white'}`}>
+            {!isBlack && (
+                <input
+                    type="text"
+                    maxLength={1} 
+                    onChange={onChange}
+                    className="square-input"
+                />
+            )}
         </div>
     );
 };
